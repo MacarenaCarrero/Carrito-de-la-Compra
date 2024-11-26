ff8123d7d415092e31e7688c8b2d726aef62c24d
Diegohhallam <macarena.c.rios@gmail.com>
2024-11-26 18:40:49 +0100

    carritoDeLaCompra

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
-const buttonsElement = document.getElementById("buttons");
+const cardsElement = document.getElementById("cards");
+const productsCart = [];
 
-const infoButton = (event) => {
-  console.log(event.target.dataset.name);
+const quantityProductIncrease = (name, addButton) => {
+  const productSelected = productsCart.find((product) => product.name === name);
+
+  productSelected.quantity++;
+  addButton.previousElementSibling.textContent = productSelected.quantity;
 };
 
-buttonsElement.addEventListener("click", infoButton);
+const addToCart = (productName, productPrice) => {
+  productsCart.push({
+    name: productName,
+    price: productPrice,
+    quantity: 1,
+  });
+  console.log(productsCart);
+};
+
+const showQuantityButton = (button) => {
+  button.classList.add("hide");
+};
+
+const productInfo = (event) => {
+  const name = event.target.dataset.name;
+  const price = event.target.dataset.price;
+  const type = event.target.dataset.type;
+  if (type === "add") {
+    showQuantityButton(event.target);
+    addToCart(name, price);
+  } else if (type === "increase") {
+    quantityProductIncrease(name, event.target);
+  }
+};
+
+cardsElement.addEventListener("click", productInfo);
